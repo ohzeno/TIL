a8b1a2d0db2a5e1cd2e411e5671d365a94d8c1a8
ohzeno <blackroseaaa3@gmail.com>
2022-03-08 00:56:58 +0900

    Add Pandas_speed_test

---
+++ Pandas/speed_test/repetition_speed.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import datetime
+
+
+def simple_for_mil(df):
+    t1 = datetime.datetime.now()
+    # 100만줄 테스트
+    for _ in range(50):
+        for i in range(length):
+            if df['고가'][i] == df_max:
+                # i 넣어도 되는데 df.index 사용하는 이유는
+                # 인덱스와 i가 다른 데이터를 취급할 경우를 가정하기 때문
+                df.at[df.index[i], '고가'] = 11111
+        for i in range(length):
+            if df['고가'][i] == 11111:
+                df.at[df.index[i], '고가'] = df_max
+    t2 = datetime.datetime.now()
+    print(f'단순 for문: {t2 - t1}')
+
+
+def iterrows_mil(df):
+    t1 = datetime.datetime.now()
+    for _ in range(50):
+        for idx, row in df.iterrows():
+            if row['고가'] == df_max:
+                df.at[idx, '고가'] = 11111
+        for idx, row in df.iterrows():
+            if row['고가'] == 11111:
+                df.at[idx, '고가'] = df_max
+    t2 = datetime.datetime.now()
+    print(f'iterrows: {t2 - t1}')
+
+
+def itertuples_mil(df):
+    t1 = datetime.datetime.now()
+    for _ in range(50):
+        for row in df.itertuples():
+            if row[5] == df_max:
+                df.at[row[0], '고가'] = 11111
+        for row in df.itertuples():
+            if row[5] == 11111:
+                df.at[row[0], '고가'] = df_max
+    t2 = datetime.datetime.now()
+    print(f'itertuples: {t2 - t1}')
+
+
+def simple_for_func(df):
+    t1 = datetime.datetime.now()
+    # 50만줄 테스트
+    for _ in range(50):
+        for i in range(length):
+            df.at[df.index[i], '고가'] = df.at[df.index[i], '저가'] * 2
+    t2 = datetime.datetime.now()
+    print(f'단순 for_50만_함수적용: {t2 - t1}')
+
+
+def iterrows_func(df):
+    t1 = datetime.datetime.now()
+    for _ in range(50):
+        for idx, row in df.iterrows():
+            df.at[idx, '고가'] = row['저가'] * 2
+    t2 = datetime.datetime.now()
+    print(f'iterrows_50만_함수적용: {t2 - t1}')
+
+
+def itertuples_func(df):
+    t1 = datetime.datetime.now()
+    for _ in range(50):
+        for row in df.itertuples():
+            df.at[row[0], '고가'] = row[6] * 2
+    t2 = datetime.datetime.now()
+    print(f'itertuples_50만_함수적용: {t2 - t1}')
+
+
+df = pd.read_csv("AAPL_10000.csv")
+length = len(df)  # 10000
+df_max = df['고가'].max()  # 최고가
+
+
+# 100만줄 if문
+simple_for_mil(df)  # 0:00:02.214019
+iterrows_mil(df)  # 0:00:17.859303
+itertuples_mil(df)  # 0:00:00.576001
+
+# 50만줄 전부 함수적용
+simple_for_func(df.copy())  # 0:00:03.104001
+iterrows_func(df.copy())  # 0:00:12.966031
+itertuples_func(df.copy())  # 0:00:02.060000
+
+t1 = datetime.datetime.now()
+for _ in range(50):
+    df['고가'] = df['저가'] * 2
+t2 = datetime.datetime.now()
+print(f'열계산: {t2 - t1}')  # 0:00:00.009009
+
+
+
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## 목차
 
 1. ### [셀레늄](Selenium/LoginBot/README.md)
+
+1. ### [판다스 속도 테스트](Pandas/speed_test/README.md)
+
