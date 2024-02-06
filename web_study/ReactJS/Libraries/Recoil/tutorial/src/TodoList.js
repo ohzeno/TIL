d35d4c74d06bd789d0f846c59d0039bddc395a62
Zeno Oh <blackroseaaa3@gmail.com>
2024-02-06 12:03:59 +0900

    Init Recoil tutorial

---
+++ web_study/ReactJS/Libraries/Recoil/tutorial/src/TodoList.js
@@ -0,0 +1,20 @@
+import { useRecoilValue } from "recoil";
+import { todoListState } from "./atoms";
+
+const TodoList = () => {
+  const todoList = useRecoilValue(todoListState);
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListFilters /> */}
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+};
+
+export default TodoList;
