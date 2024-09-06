03769f2fadba8ad61ad02ee3e5b26ef7ea76cf69
Zeno Oh <blackroseaaa3@gmail.com>
2024-09-06 15:40:25 +0900

    Add unit test for UserController's createUser method

--- web_study/NestJS/ntest/src/user/controllers/user.controller.spec.ts
+++ web_study/NestJS/ntest/src/user/controllers/user.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from '../services/user.service';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { User } from '../user.entity';
+
+jest.mock('../services/user.service');
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: jest.Mocked<UserService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should create a new user', async () => {
+      const createUserDto: CreateUserDto = { name: 'Test User', age: 30 };
+      const expectedUser: User = { id: '1', ...createUserDto };
+
+      userService.createUser.mockResolvedValue(expectedUser);
+
+      const result = await controller.createUser(createUserDto);
+
+      expect(userService.createUser).toHaveBeenCalledWith(createUserDto);
+      expect(result).toEqual(expectedUser);
+    });
   });
 });
